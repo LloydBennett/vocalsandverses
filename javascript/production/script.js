632d3fc31524d33ca2825f61ca88017e92a16e4a
Lloyd-Jamie Bennett <lloydbennett@Lloyd-Jamies-MacBook-Pro.local>
2017-10-17 13:44:23 +0100

    add large image gallery modal image

--- javascript/production/script.js
+++ javascript/production/script.js
@@ -1,16 +1,3 @@
-//dom elements
-
-/*var $pageTitle = document.querySelector('head title'),
-	$testimonals = document.querySelectorAll('.testimonals'),
-	$testimonyQuotes = document.querySelectorAll('.entry'),
-	$testimonyImages = document.querySelectorAll('.carousel-image .thumbnail'),
-	$prevController = document.querySelector('.prev'),
-	$nextController = document.querySelector('.next'),
-	$modalOverlay = document.querySelector('.modal-overlay'),
-	$modalWindow = document.querySelectorAll('.modal-content'),
-	$largePlayBtn = document.querySelector('.play-btn-lrg')*/
-//var $testimonals = document.querySelectorAll('[data-carousel="testimonal"]');
-
 function removeClassFromNodeList(nodeList, className){
   if(typeof className === "object") {
     nodeList.forEach(function(element){
@@ -65,17 +52,8 @@ function setupDefaultCarousels() {
 
 	//initialises all functions that need to be called
 	function init(){
-
     setupGalleryModal();
     setupDefaultCarousels();
-
-		// var pageTransition = new PageTransition(document.querySelectorAll('[data-page-transition]'));
-		// var navMenu = new NavigationMenu({
-		// 	menu: document.querySelector('[data-role="nav-menu"]'),
-		// 	menuTrigger: document.querySelector('[data-role="open-menu"]'),
-		// 	body: document.querySelector('body'),
-		// 	navLinks: document.querySelectorAll('.nav-modal [data-role="nav-menu-links"]')
-		// });
 	}
 
 	window.onload = function() {
